8f2a392940c4e78c2d51314c9fb448e915f6425c
Bartek <bartekp44@gmail.com>
2017-07-30 00:57:35 +0200

    Rework jump scares

---
+++ Assets/Scripts/JumpScares/SoundJumpScare.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundJumpScare : JumpScareBase
+{
+	void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+	
+	void Update ()
+    {
+		
+	}
+    
+
+    public override void OnJumpScareExecution()
+    {
+        if (collidedObject.gameObject.CompareTag("Player"))
+        {
+            jumpsScareDone = true;
+            audioSource.Play();
+            collidedObject.gameObject.GetComponent<PlayerController>().sanityContr.DealSanityDamage(5f);
+        }
+    }
+}

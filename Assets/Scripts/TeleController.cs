d10576a5bcc0352e060926f7cd190c0e832c5e04
Xsior Florczyk <xsior95@gmail.com>
2017-07-30 21:04:23 +0200

    TeleMindFuck

---
+++ Assets/Scripts/TeleController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleController : MonoBehaviour {
+    public Transform offset;
+    public Transform destination;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Vector3 off = (offset.position - collision.transform.position);
+            collision.transform.position = destination.position - off;
+        }
+    }
+}

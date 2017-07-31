533476bf0be2b2547fbe777146d2e4cdc3f9c01a
Xsior <195583@edu.p.lodz.pl>
2017-07-31 19:07:31 +0200

    Some mao changes
    someone broke enemy but new unity run like shit for me so gotta find way
    before i fix that shit.

--- Assets/Scripts/TeleController.cs
+++ Assets/Scripts/TeleController.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class TeleController : MonoBehaviour {
     public Transform offset;
     public Transform destination;
-
+    public int keyNeeded;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.GetComponent<PlayerController>() != null)
         {
-            Vector3 off = (offset.position - collision.transform.position);
-            collision.transform.position = destination.position - off;
+            if (KeyCollector.CheckForKey(keyNeeded))
+            {
+                Vector3 off = (offset.position - collision.transform.position);
+                collision.transform.position = destination.position - off;
+            }
+            else
+            {
+                collision.transform.position -= new Vector3(10,0,0);
+            }
         }
     }
 }

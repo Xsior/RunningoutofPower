17b01b32779c630ec0fe56e2ff821f6c3f2c596e
Xsior <195583@edu.p.lodz.pl>
2017-08-01 02:09:02 +0200

    finish

--- Assets/FinishLine.cs
+++ Assets/FinishLine.cs
@@ -3,46 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class FinishLine : MonoBehaviour
 {
-    float timer = 1f;
-    bool chuj = true;
-	// Use this for initialization
-	void Start ()
-    {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-        if (timer > 0)
-        {
-            timer -= Time.deltaTime;
-        }
-        else
-        {
-            timer = 1f;
-            if(chuj)
-            {
-                transform.Find("NewSprite").GetComponent<SpriteRenderer>().DOFade(0, 1);
-                chuj = !chuj;
-            }
-            else
-            {
-                transform.Find("NewSprite").GetComponent<SpriteRenderer>().DOFade(1, 1);
-                chuj = !chuj;
-            }
-            
-        }
-    }
+    bool chuj = false;
+    public Image image;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene("MainMenu");
+            image.DOFade(1, 1).OnComplete(() => SceneManager.LoadScene("MainMenu") );
         }
         
 
